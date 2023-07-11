762de128b7181257c26f74f7a0e7eff334cdddcf
LouisOnraed <louisonraed59@gmail.com>
2023-07-11 16:10:20 +0200

    Création de la slash command ping

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,8 +1,11 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log('####################################');
         console.log('Je suis prêt !');
+
+        const devGuild = await client.guilds.cache.get('1126847075141824583');
+        devGuild.commands.set(client.commands.map(cmd => cmd));
     }
 }
