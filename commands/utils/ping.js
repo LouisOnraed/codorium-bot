74f44ca4482c963189c7609621b10f018a302f68
LouisOnraed <louisonraed59@gmail.com>
2023-07-11 16:45:26 +0200

    correction erreur + supression .env

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,31 +1,31 @@
-const { messageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'ping',
     description: 'Commande ping!',
     run: (client, message, args) => {
-        const embed = new messageEmbed()
+        const embed = new MessageEmbed()
             .setTitle('🏓 Pong !')
-            .setThumbail(client.user.displayAvatarURL())
+            .setThumbnail(client.user.displayAvatarURL())
             .addFields(
                 { name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true },
-                { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true },
+                { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true }
             )
             .setTimestamp()
-            .setFooter({ text: message.author.name, iconURL: message.author.displayAvatarURL() });
+            .setFooter({ text: message.author.username, iconURL: message.author.displayAvatarURL() });
 
         message.channel.send({ embeds: [embed] });
     },
     runSlash: (client, interaction) => {
-        const embed = new messageEmbed()
+        const embed = new MessageEmbed()
             .setTitle('🏓 Pong !')
-            .setThumbail(client.user.displayAvatarURL())
+            .setThumbnail(client.user.displayAvatarURL())
             .addFields(
                 { name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true },
-                { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true },
+                { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true }
             )
             .setTimestamp()
-            .setFooter({ text: interaction.user.name, iconURL: interaction.user.displayAvatarURL() });
+            .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() });
 
         interaction.reply({ embeds: [embed] });
     }
