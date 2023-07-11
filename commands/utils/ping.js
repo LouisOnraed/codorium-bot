0c04c95cc181fa80aa7da82c9c2354a4966dd42b
LouisOnraed <louisonraed59@gmail.com>
2023-07-11 16:23:45 +0200

    Passage aux embeds

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,10 +1,32 @@
+const { messageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'ping',
     description: 'Commande ping!',
     run: (client, message, args) => {
-        message.channel.send('Pong !');
+        const embed = new messageEmbed()
+            .setTitle('🏓 Pong !')
+            .setThumbail(client.user.displayAvatarURL())
+            .addFields(
+                { name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true },
+                { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true },
+            )
+            .setTimestamp()
+            .setFooter({ text: message.author.name, iconURL: message.author.displayAvatarURL() });
+
+        message.channel.send({ embeds: [embed] });
     },
     runSlash: (client, interaction) => {
-        interaction.reply('Pong !');
+        const embed = new messageEmbed()
+            .setTitle('🏓 Pong !')
+            .setThumbail(client.user.displayAvatarURL())
+            .addFields(
+                { name: 'Latence', value: `\`${client.ws.ping}ms\``, inline: true },
+                { name: 'Uptime', value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true },
+            )
+            .setTimestamp()
+            .setFooter({ text: interaction.user.name, iconURL: interaction.user.displayAvatarURL() });
+
+        interaction.reply({ embeds: [embed] });
     }
 }
