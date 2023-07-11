b42c0694514d2aee3d13d53b23f58294646863fc
LouisOnraed <louisonraed59@gmail.com>
2023-07-11 15:27:47 +0200

    Ajout de log et process

--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -5,6 +5,11 @@ const pGlob = promisify(glob);
 module.exports = async client => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async commandFile => {
         const cmd = require(commandFile);
+
+        if(!cmd.name) return console.log(`------\nCommande non-chargée: pas de nom)\nFichier -> ${commandFile}\n------`)
+
         client.commands.set(cmd.name, cmd);
+
+        console.log(`✅ Commande chargée avec succès : ${cmd.name}`);
     })
 }
