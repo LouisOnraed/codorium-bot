7329a7488a19aa388b7daa578b49d9014203ec2c
LouisOnraed <louisonraed59@gmail.com>
2023-07-11 18:05:30 +0200

    Connection à la base de données

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
 const client = new Client({ intents: 3276799 });
 
 client.commands = new Collection();
@@ -11,5 +12,14 @@ process.on('exit', code => {console.log(`Le processus s'est arrêté avec le cod
 process.on('uncaughtException', (err, origin) => {console.log(`UNCAUGHT_EXCEPTION: ${err}`, `Origine: ${origin}`) });
 process.on('unhandledRejection', (reason, promise) => {console.log(`UNHANDLED_REJECTION: ${reason}\n-----\n`, promise) });
 process.on('warning', (...args) => console.log(...args));
+
+mongoose.connect(process.env.DATABASE_URI, {
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+}).then(() => { console.log('Le client est connecté à la base de donnée') })
+.catch(err => { console.log(err); });
 
 client.login(process.env.DISCORD_TOKEN);
